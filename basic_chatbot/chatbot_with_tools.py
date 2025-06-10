fa3487a5185d306a04be9ac7301fc5ad24e53ee7
Samuel Raj Bhat <20534771-samuelrajbhat@users.noreply.gitlab.com>
2025-06-10 12:17:23 +0545

    second basic chatbot without memory features

--- basic_chatbot/chatbot_with_tools.py
+++ basic_chatbot/chatbot_with_tools.py
@@ -11,13 +11,51 @@ from langgraph.prebuilt import ToolNode
 
 
 load_dotenv()
-groq_model = os.getenv("groq_model")
 
-llm = ChatGroq(model= groq_model)
+class  BasicChatBot(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+groq_model = os.getenv("groq_model")
 
 search_tool = TavilySearchResults(max_results = 2)
 tools = [search_tool]
 
+llm = ChatGroq(model= groq_model)
 llm_with_tools = llm.bind_tools(tools = tools)
-result = llm_with_tools.invoke("whats the weather in kathmandu?")
-print(result)
+
+def chatbot(state: BasicChatBot):
+    return{
+        "messages": [llm_with_tools.invoke(state["messages"])]
+    }
+
+def tools_router(state: BasicChatBot):
+    last_message = state["messages"][-1]
+
+    if(hasattr(last_message, "tool_calls") and len(last_message.tool_calls)> 0):
+        return "tool_node"
+    else:
+        return END
+    
+
+tool_node = ToolNode(tools= tools)
+
+graph = StateGraph(BasicChatBot)
+
+graph.add_node("chatbot", chatbot)
+graph.add_node("tool_node", tool_node)
+
+graph.set_entry_point("chatbot")
+graph.add_conditional_edges("chatbot", tools_router)
+graph.add_edge("tool_node", "chatbot")
+app = graph.compile()
+
+while True:
+    user_input = input("user: ")
+    if(user_input in ["exit", "end"]):
+        break
+    else:
+        result = app.invoke({
+            "messages": [HumanMessage(content= user_input)]
+        })
+    print(result)
